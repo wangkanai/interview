83f535d7d1b6185f31585efdc3f3e0e8327f125c
wangkanai <sarin@wangkanai.com>
2025-06-19 09:36:14 +0700

    Refactor `HealthCheckAnnotation` for readability and alignment with coding standards

--- Aspire/AppHost/Hosting/‎HealthCheckAnnotation.cs
+++ Aspire/AppHost/Hosting/‎HealthCheckAnnotation.cs
@@ -6,23 +6,17 @@ namespace Wangkanai.Interview.AppHost.Hosting;
 
 public class HealthCheckAnnotation(Func<IResource, CancellationToken, Task<IHealthCheck?>> healthCheckFactory) : IResourceAnnotation
 {
-   public Func<IResource, CancellationToken, Task<IHealthCheck?>> HealthCheckFactory { get; } = healthCheckFactory;
+	public Func<IResource, CancellationToken, Task<IHealthCheck?>> HealthCheckFactory { get; } = healthCheckFactory;
 
-   public static HealthCheckAnnotation Create(Func<string, IHealthCheck> connectionStringFactory)
-   {
-      return new(async (resource, token) =>
-      {
-         if (resource is not IResourceWithConnectionString c)
-         {
-            return null;
-         }
+	public static HealthCheckAnnotation Create(Func<string, IHealthCheck> connectionStringFactory)
+	{
+		return new(async (resource, token) => {
+			if (resource is not IResourceWithConnectionString c)
+				return null;
 
-         if (await c.GetConnectionStringAsync(token) is not string cs)
-         {
-            return null;
-         }
-
-         return connectionStringFactory(cs);
-      });
-   }
+			return await c.GetConnectionStringAsync(token) is not { } cs
+				       ? null
+				       : connectionStringFactory(cs);
+		});
+	}
 }
